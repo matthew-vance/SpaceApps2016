048308103b98ab520e2cc4a5f6f162cc6c0be8bb
acodemonkey <bnparsons@gmail.com>
2016-04-23 14:39:56 -0400

    saving progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,25 @@
-alert('LOADED A THING!!!');
+//initialize app
+angular.module('app', []);
+
+angular.module('app').controller('MajorTom', [$window, function(){
+  
+  var bus = this;
+  bus.message = "Space Apps 2016 Space Tour!";
+
+  bus.items = [ 'museum',
+  'restaurant',
+  'landmark',
+  'theater',
+  'meteorite crater'
+  ];
+
+  var map = $window.L.map('map');
+  bus.MapModel = {
+    map: map,
+    basemaps : basemaps,
+    layers: layers,
+    zoom: 8
+
+  }
+
+}]);
