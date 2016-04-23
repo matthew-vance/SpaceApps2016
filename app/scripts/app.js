c64e4d7ee0d81ffee396c21b2e8b7354356bed57
Matthew Vance <mavance44@gmail.com>
2016-04-23 15:46:30 -0400

    added stuff for markers and whatnot

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,32 @@
 //initialize app
-angular.module('app', ['leaflet-directive']);
+angular.module('app', ['leaflet-directive'])
+    .controller('MajorTom', MainCtrl);
 
-angular.module('app').controller('MajorTom', MainCtrl);
-
-function MainCtrl (){
+function MainCtrl() {
     var bus = this;
     bus.message = "Space Apps 2016 Space Tour!";
 
-    bus.items = [ 'museum',
-    'restaurant',
-    'landmark',
-    'theater',
-    'meteorite crater'
+    bus.items = ['museum',
+        'restaurant',
+        'landmark',
+        'theater',
+        'meteorite crater'
     ];
-}
+    bus.center = {
+        lat: 59.91,
+        lng: 10.75,
+        zoom: 12
+    };
+    bus.markers = {
+        marker: {
+            lat: 59.91,
+            lng: 10.75,
+            message: "I want to travel here!",
+            focus: true,
+            draggable: false
+        }
+    };
+    bus.defaults = {
+        scrollWheelZoom: false
+    };
+};
